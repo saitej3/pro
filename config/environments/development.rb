31fc1d26166b44de9092b6548e01de6e910c1cca
saitej3 <saitej3@gmail.comm>
2014-12-18 19:49:15 +0530

    t

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = "sai,,,,,,,,,,"
+
   config.action_mailer.default_url_options = { host: host }
 
   # Print deprecation notices to the Rails logger.
